36c3c0fe142da104d8951b32beac727cda894a3f
Cameron Horejs <cameron.horejs@gmail.com>
2019-06-19 22:11:17 -0700

    Initial commit containing index and comment detail component

---
+++ components/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+
+import CommentDetail from './CommentDetail';
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" commentText= "I like potatoes!" thumbIMG={faker.image.avatar()}/>        
+      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" commentText="I like 'em a lot." thumbIMG={faker.image.avatar()}/>       
+      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" commentText="I like 'em cold..." thumbIMG={faker.image.avatar()}/>        
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
