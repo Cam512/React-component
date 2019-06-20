4bc7aaaa8d854b931d7e56545e3f4d584e3d0e2b
Cameron Horejs <cameron.horejs@gmail.com>
2019-06-19 22:59:40 -0700

    Added comment card component

--- components/src/index.js
+++ components/src/index.js
@@ -2,14 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 
+import ApprovalCard from './ApprovalCard';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" commentText= "I like potatoes!" thumbIMG={faker.image.avatar()}/>        
-      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" commentText="I like 'em a lot." thumbIMG={faker.image.avatar()}/>       
-      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" commentText="I like 'em cold..." thumbIMG={faker.image.avatar()}/>        
+      <ApprovalCard>
+      <CommentDetail 
+      author="Sam" 
+      timeAgo="Today at 4:45PM" 
+      commentText= "I like potatoes!" 
+      thumbIMG={faker.image.avatar()}/> 
+      </ApprovalCard>       
+      
+      <CommentDetail 
+      author="Alex" 
+      timeAgo="Today at 2:00AM" 
+      commentText="I like 'em a lot." 
+      thumbIMG={faker.image.avatar()}/>       
+      
+      <CommentDetail 
+      author="Jane" 
+      timeAgo="Yesterday at 5:00PM" 
+      commentText="I like 'em cold..." 
+      thumbIMG={faker.image.avatar()}/>        
+
+      
     </div>
   );
 };
