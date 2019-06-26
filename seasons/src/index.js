cd89e645a1fcaacb5b0b018163df3bd1f6c9dec1
cambo512 <cameron.horejs@gmail.com>
2019-06-25 23:06:17 -0700

    added season display logic and styling

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import SeasonDisplay from './SeasonDisplay';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -26,7 +28,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-        return <div>Lattitude: {this.state.lat}</div>
+        return <SeasonDisplay lat={this.state.lat} />
     }
 
     return <div>Loading... </div>;
