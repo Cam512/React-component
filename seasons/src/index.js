575ca686356fb1f421f60131486c3aa24f780163
cambo512 <cameron.horejs@gmail.com>
2019-06-29 22:27:27 -0700

    Created render helper method for adding custom styling

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -16,24 +16,33 @@ class App extends React.Component {
 
   }
 
-  componentDidMount () {
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-        position => this.setState({ lat: position.coords.latitude }),
-        err => this.setState({ errorMessage: err.message })
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
-        return <div>Error: {this.state.errorMessage}</div>
+      return <div>Error: {this.state.errorMessage}</div>
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-        return <SeasonDisplay lat={this.state.lat} />
+      return <SeasonDisplay lat={this.state.lat} />
     }
 
     return <Loader message="Please accept location request" />;
-    
-  }
 }
+
+
+render() {
+  return (
+    <div className="border red">
+      {this.renderContent()}
+    </div>
+  );
+ }
+}
+
 ReactDOM.render(<App />, document.querySelector("#root"));
