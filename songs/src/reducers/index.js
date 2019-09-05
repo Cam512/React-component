7d7f86b46671f3544638d8508c0ad5a25b7bd671
cambo512 <cameron.horejs@gmail.com>
2019-09-04 20:40:37 -0700

    had issue with config files, deleted then reinstalled then added redux configuration

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+    return [
+        { title: 'Borderlines', duration: '5:30' },
+        { title: 'Honecomb', duration: '12:00'},
+        { title: 'Jake Leg', duration: '5:00'},
+        { title: 'Dream House', duration: '9:45'}
+    ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+
+    return selectedSong;
+};
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
